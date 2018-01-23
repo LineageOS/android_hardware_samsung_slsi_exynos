f15701964b187de98f51f1831b7d6e06e2afe9e0
Christopher N. Hesse <raymanfx@gmail.com>
2018-01-24 00:35:25 +0100

    libstagefrighthw: Add liblog dependency
    Change-Id: I2579c6770c7b30c3bc10cc14aa3485f3917ca852

--- libstagefrighthw/Android.mk
+++ libstagefrighthw/Android.mk
@@ -31,7 +31,8 @@ LOCAL_SHARED_LIBRARIES :=    \
         libcutils            \
         libui                \
         libdl                \
-        libstagefright_foundation
+        libstagefright_foundation \
+        liblog
 
 LOCAL_MODULE := libstagefrighthw
 
